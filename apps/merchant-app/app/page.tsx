4dbd350e8d6fcec485c8264920a745ee71ffc9a3
Pranshu Verma <pranshuverma1601@gmail.com>
2024-04-21 21:20:16 +0530

    created schemas

--- apps/merchant-app/app/page.tsx
+++ apps/merchant-app/app/page.tsx
@@ -2,7 +2,9 @@
 
 import { useBalance } from "@repo/store/balance";
 
-export default function () {
+export default function() {
   const balance = useBalance();
-  return <div>hi there {balance}</div>;
+  return <div>
+    hi there {balance}
+  </div>
 }
--- apps/user-app/app/lib/auth.ts
+++ apps/user-app/app/lib/auth.ts
@@ -1,4 +1,4 @@
-import db from "@repo/db/client";
+import prisma from "@repo/db/client";
 import CredentialsProvider from "next-auth/providers/credentials";
 import bcrypt from "bcrypt";
 
@@ -7,21 +7,19 @@ export const authOptions = {
     CredentialsProvider({
       name: "Credentials",
       credentials: {
-        phone: {
-          label: "Phone number",
-          type: "text",
-          placeholder: "1231231231",
-          required: true,
-        },
+        email: { label: "Email", type: "email", required: true },
+        phone: { label: "Phone number", type: "text", required: true },
         password: { label: "Password", type: "password", required: true },
+        name: { label: "Name", type: "text", required: true },
       },
       // TODO: User credentials type from next-auth
       async authorize(credentials: any) {
         // Do zod validation, OTP validation here
         const hashedPassword = await bcrypt.hash(credentials.password, 10);
-        const existingUser = await db.user.findFirst({
+        const existingUser = await prisma.user.findFirst({
           where: {
             number: credentials.phone,
+            email: credentials.email,
           },
         });
 
@@ -34,24 +32,28 @@ export const authOptions = {
             return {
               id: existingUser.id.toString(),
               name: existingUser.name,
-              email: existingUser.number,
+              email: existingUser.email,
+              number: existingUser.number,
             };
           }
           return null;
         }
 
         try {
-          const user = await db.user.create({
+          const newUser = await prisma.user.create({
             data: {
               number: credentials.phone,
               password: hashedPassword,
+              email: credentials.email,
+              name: credentials.name,
             },
           });
 
           return {
-            id: user.id.toString(),
-            name: user.name,
-            email: user.number,
+            id: newUser.id.toString(),
+            name: newUser.name,
+            email: newUser.email,
+            number: newUser.number,
           };
         } catch (e) {
           console.error(e);
